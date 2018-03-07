62fc472a9f694cafb636a79c6661c81eb77b6b24
jakeprem <jakeprem@gmail.com>
2018-03-07 16:43:00 -0500

    Extract TalkForm to its own component

---
+++ src/components/TalkForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Submit a Lightning Talk</h1>
+      <h2 class="subtitle is-size-6 has-text-grey-light">Deadline: {{ submissionDeadline }}</h2>
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input type="text" class="input" v-model="inputTitle" placeholder="Enter your title here">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Abstract</label>
+        <div class="control">
+          <textarea class="textarea" v-model="inputAbstract" placeholder="Enter your abstract here"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Outline/Slides</label>
+        <div class="control">
+          <input type="text" class="input" v-model="inputOutline" placeholder="Enter a link here">
+        </div>
+      </div>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link" @click="submitTalk">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-text" @click="resetInputs">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import { format } from '@/utils/dates'
+
+export default {
+  name: 'SubmitPage',
+  data () {
+    return {
+      inputTitle: '',
+      inputAbstract: '',
+      inputOutline: ''
+    }
+  },
+  methods: {
+    ...mapActions([
+      'submitTalkForEvent'
+    ]),
+    resetInputs () {
+      this.inputTitle = ''
+      this.inputAbstract = ''
+      this.inputOutline = ''
+    },
+    submitTalk () {
+      this.submitTalkForEvent({
+        title: this.inputTitle,
+        abstract: this.inputAbstract,
+        outline_link: this.inputOutline
+      }).then(x => {
+        console.log(x)
+        this.resetInputs()
+      })
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'activeEvent'
+    ]),
+    submissionDeadline () {
+      return format.monthDayYear(this.activeEvent.submissionDeadline)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/SubmitPage.vue
+++ src/pages/SubmitPage.vue
@@ -1,81 +1,17 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <h1 class="title">Submit a Lightning Talk</h1>
-      <h2 class="subtitle is-size-6 has-text-grey-light">Deadline: {{ submissionDeadline }}</h2>
-      <div class="field">
-        <label class="label">Title</label>
-        <div class="control">
-          <input type="text" class="input" v-model="inputTitle" placeholder="Enter your title here">
-        </div>
-      </div>
-      <div class="field">
-        <label class="label">Abstract</label>
-        <div class="control">
-          <textarea class="textarea" v-model="inputAbstract" placeholder="Enter your abstract here"></textarea>
-        </div>
-      </div>
-      <div class="field">
-        <label class="label">Outline/Slides</label>
-        <div class="control">
-          <input type="text" class="input" v-model="inputOutline" placeholder="Enter a link here">
-        </div>
-      </div>
-      <div class="field is-grouped">
-        <div class="control">
-          <button class="button is-link" @click="submitTalk">Submit</button>
-        </div>
-        <div class="control">
-          <button class="button is-text" @click="resetInputs">Cancel</button>
-        </div>
-      </div>
-    </div>
-  </section>
+  <talk-form></talk-form>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import { format } from '@/utils/dates'
+import TalkForm from '@/components/TalkForm'
 
 export default {
-  name: 'SubmitPage',
-  data () {
-    return {
-      inputTitle: '',
-      inputAbstract: '',
-      inputOutline: ''
-    }
-  },
-  methods: {
-    ...mapActions([
-      'submitTalkForEvent'
-    ]),
-    resetInputs () {
-      this.inputTitle = ''
-      this.inputAbstract = ''
-      this.inputOutline = ''
-    },
-    submitTalk () {
-      this.submitTalkForEvent({
-        title: this.inputTitle,
-        abstract: this.inputAbstract,
-        outline_link: this.inputOutline
-      }).then(x => {
-        console.log(x)
-        this.resetInputs()
-      })
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'activeEvent'
-    ]),
-    submissionDeadline () {
-      return format.monthDayYear(this.activeEvent.submissionDeadline)
-    }
+  components: {
+    TalkForm
   }
 }
 </script>
 
 <style>
+
 </style>
